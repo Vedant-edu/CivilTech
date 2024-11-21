c7c2522e3e2e650ae41ada7f0bb7645de3a6c52a
TheVedant <vedantmute2004@gmail.com>
2024-11-21 16:52:35 +0530

    first commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -60,7 +60,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-black">
-          <p>&copy; {new Date().getFullYear()} Civil Tech. All rights reserved.</p>
+          <p>&copy; {new Date().getFullYear()} Civil Tech. All rights reserved. | Developed By Vedant</p>
         </div>
       </div>
     </footer>
